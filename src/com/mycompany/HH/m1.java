59798a9f7f5dc003a5fa984db620e7fc976dc004
Rayleigh <rayleigh190@gamil.com>
2021-07-14 22:33:54 +0900

    first commit

---
+++ src/com/mycompany/HH/m1.java
@@ -0,0 +1,88 @@
+package com.mycompany.HH;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+import android.app.*;
+import android.view.View.OnClickListener.*;
+import android.widget.Button.*;
+import android.content.*;
+import android.net.*;
+import android.app.Activity.*;
+import android.media.MediaPlayer.*;
+import android.os.Bundle.*;
+import android.app.Activity.*;
+import android.app.AlertDialog.*;
+import android.content.DialogInterface.*;
+import android.os.Bundle.*;
+import android.view.View.*;
+import android.view.View.OnClickListener.*;
+import android.widget.Button.*;
+import android.webkit.*;
+import android.media.*;
+import android.widget.Toast.*;
+import android.app.Activity;
+import android.widget.EditText.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import android.app.Activity;
+import android.content.Context;
+import android.database.SQLException;
+import android.os.Bundle;
+import android.provider.ContactsContract.CommonDataKinds.Im;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.renderscript.*;
+import android.widget.*;
+
+
+public class m1 extends Activity
+{
+
+
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+	    setContentView(R.layout.m1);
+
+		{Button Button1 = (Button) findViewById(R.id.Button7);
+
+			Button1.setOnClickListener(new OnClickListener(){ public void onClick(View v){
+						Intent i=new Intent(m1.this,m3.class);
+						startActivity(i);}			
+					{Button Button1 = (Button) findViewById(R.id.Button8);
+
+						Button1.setOnClickListener(new OnClickListener(){ public void onClick(View v){
+									Intent i=new Intent(m1.this,m4.class);
+									startActivity(i);}
+
+								{Button Button1 = (Button) findViewById(R.id.Button9);
+
+									Button1.setOnClickListener(new OnClickListener(){ public void onClick(View v){
+												Intent i=new Intent(m1.this,m5.class);
+												startActivity(i);}
+		
+	});
+}});}});}}}
